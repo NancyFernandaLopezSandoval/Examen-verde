2dc00de2757903ec50e3a9a81d13d47f00a0a8b7
NancyFernandaLopezSandoval <111475171+NancyFernandaLopezSandoval@users.noreply.github.com>
2024-06-19 11:02:35 -0600

    Add files via upload

---
+++ a.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Edge()
+driver.get("https://www.udemy.com/")
+
+webElement = driver.find_element(By.ID,"u201-popper-trigger--110").click()
+
+
+time.sleep(50)
